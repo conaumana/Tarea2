3a6c402c6ff7564b9348b684ae37bd3a4538f962
conaumana <constanza.umana@uc.cl>
2024-11-03 12:44:05 -0300

    p1.py

---
+++ p1.py
@@ -0,0 +1,15 @@
+# p1.py
+import sys
+from read import read
+from plot import plot1D, plot2D
+
+if __name__ == "__main__":
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    data = read(input_file)
+    if len(data) == 2:
+        plot1D(data[0], data[1], "Tiempo", "Posición")
+    elif len(data) == 3:
+        plot2D(data[1], data[2], data[0], "x", "y")
+    plt.savefig(output_file)
