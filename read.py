54c6b611f6df4f311ed6e853bf4528107161f783
conaumana <constanza.umana@uc.cl>
2024-11-04 16:21:28 -0300

    read.py

--- read.py
+++ read.py
@@ -2,20 +2,20 @@
 import csv
 
 def read(filename):
-    time = []
-    values1 = []
-    values2 = []
+    instancia = []
+    posicion1 = []
+    posicion2 = []
 
     with open(filename, 'r') as file:
         reader = csv.reader(file)
         next(reader)  # Saltar la cabecera
         for row in reader:
-            time.append(float(row[0]))
-            values1.append(float(row[1]))
+            instancia.append(float(row[0]))
+            posicion1.append(float(row[1]))
             if len(row) > 2:
-                values2.append(float(row[2]))
+                posicion2.append(float(row[2]))
 
     if values2:
-        return time, values1, values2
+        return instancia, posicion1, posicion2
     else:
-        return time, values1
+        return instancia, posicion1
