fd9d0f61c313b7eb74f5b5ab8194c1449cda7853
conaumana <constanza.umana@uc.cl>
2024-11-04 18:08:10 -0300

    p1.py

--- p1.py
+++ p1.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     data = read(input_file)
     if len(data) == 2:
         plot1D(data[0], data[1], "Tiempo", "Posición")
+        plt.savefig(output_file)
     elif len(data) == 3:
         plot2D(data[1], data[2], data[0], "x", "y")
-    plt.savefig(output_file)
+        plt.savefig(output_file)
