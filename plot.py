fbfd6800efa7f1b6ac107b53340863708a56947c
conaumana <constanza.umana@uc.cl>
2024-11-04 16:59:49 -0300

    plot.py

--- plot.py
+++ plot.py
@@ -1,19 +1,19 @@
 # plot.py
 import matplotlib.pyplot as plt
 
-def plot1D(x, y, xlabel, ylabel):
+def plot1D(x, y, ejeX, ejeY):
     plt.figure()
     plt.plot(x, y)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+    plt.xlabel(ejeX)
+    plt.ylabel(ejeY)
     plt.savefig("out1d.png")
     plt.close()
 
-def plot2D(x, y, c, xlabel, ylabel):
+def plot2D(x, y, c, ejeX, ejeY):
     plt.figure()
     plt.scatter(x, y, c=c, cmap='viridis')
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+    plt.xlabel(ejeX)
+    plt.ylabel(ejeY)
     plt.colorbar(label="Tiempo")
     plt.savefig("out2d.png")
     plt.close()
