2f9dea397de9c9e14a7842e046489fb408710a08
conaumana <constanza.umana@uc.cl>
2024-11-04 17:03:33 -0300

    read.py

--- read.py
+++ read.py
@@ -2,7 +2,7 @@
 import csv
 
 def read(filename):
-    tiempo = []
+    Tiempo = []
     posicion1 = []
     posicion2 = []
 
@@ -10,12 +10,12 @@ def read(filename):
         reader = csv.reader(file)
         next(reader)  # Saltar la cabecera
         for row in reader:
-            tiempo.append(float(row[0]))
+            Tiempo.append(float(row[0]))
             posicion1.append(float(row[1]))
             if len(row) > 2:
                 posicion2.append(float(row[2]))
 
     if values2:
-        return tiempo, posicion1, posicion2
+        return Tiempo, posicion1, posicion2
     else:
-        return tiempo, posicion1
+        return Tiempo, posicion1
